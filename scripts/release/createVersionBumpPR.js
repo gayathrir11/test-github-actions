9322efc695400e7158e181df1330f836c44f51d5
Gayathri <rallapalle.gayathrichowdary@ny.email.gs.com>
2023-12-11 18:01:08 +0530

    update script

--- scripts/release/createVersionBumpPR.js
+++ scripts/release/createVersionBumpPR.js
@@ -1,21 +1,20 @@
-import * as github from '@actions/github';
-import * as githubActionCore from '@actions/core';
-import chalk from 'chalk';
-import { execSync } from 'child_process';
+import * as github from "@actions/github";
+import * as githubActionCore from "@actions/core";
+import chalk from "chalk";
+import { execSync } from "child_process";
 
-const DEFAULT_BRANCH_NAME = 'main';
-const PREPARE_RELEASE_PR_BRANCH_NAME = 'bot/prepare-release';
+const DEFAULT_BRANCH_NAME = "main";
+const PREPARE_RELEASE_PR_BRANCH_NAME = "bot/prepare-release";
 
 const prepareNewStandardRelease = async () => {
   const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
   const bumpType = process.env.BUMP_TYPE;
-  const PACKAGE_JSON_PATH = 'package.json';
+  const PACKAGE_JSON_PATH = "package.json";
 
   // Push release version bump changeset
   console.log(`Creating release version bump PR...`);
 
   try {
-    
     const defaultBranchRef = (
       await octokit.rest.git.getRef({
         ref: `heads/${DEFAULT_BRANCH_NAME}`,
@@ -40,7 +39,7 @@ const prepareNewStandardRelease = async () => {
     });
 
     // Use npm version to update the version based on the specified type
-    execSync(`npm version ${bumpType}`, { stdio: 'inherit' });
+    execSync(`npm version ${bumpType}`, { stdio: "inherit" });
 
     let existingChangesetFile;
     let existingChangesetFileContent;
@@ -51,17 +50,18 @@ const prepareNewStandardRelease = async () => {
           ...github.context.repo,
         })
       ).data;
-      existingChangesetFileContent = Buffer.from(existingChangesetFile.data.content, 'base64').toString('utf-8');
+      existingChangesetFileContent = Buffer.from(
+        existingChangesetFile,
+        "base64"
+      ).toString("utf-8");
     } catch (error) {
-      githubActionCore.error(
-        `Failed to find file. Error:\n${error.message}\n`,
-      );
+      githubActionCore.error(`Failed to find file. Error:\n${error.message}\n`);
       process.exit(1);
     }
 
     await octokit.rest.repos.createOrUpdateFileContents({
       path: PACKAGE_JSON_PATH,
-      message: 'prepare for new release',
+      message: "prepare for new release",
       branch: PREPARE_RELEASE_PR_BRANCH_NAME,
       content: existingChangesetFile,
       // 'sha' is required when we update the file, i.e the changeset file exists but its content is stale
@@ -73,7 +73,7 @@ const prepareNewStandardRelease = async () => {
     const bumpVersionPR = (
       await octokit.rest.pulls.create({
         title: `Prepare New ${
-          bumpType === 'major' ? 'Release' : 'Iteration Release'
+          bumpType === "major" ? "Release" : "Iteration Release"
         }`,
         head: PREPARE_RELEASE_PR_BRANCH_NAME,
         base: DEFAULT_BRANCH_NAME,
@@ -82,12 +82,12 @@ const prepareNewStandardRelease = async () => {
     ).data;
     console.log(
       chalk.green(
-        `\u2713 Created a PR to push release version bump : ${bumpVersionPR.html_url}`,
-      ),
+        `\u2713 Created a PR to push release version bump : ${bumpVersionPR.html_url}`
+      )
     );
   } catch (error) {
     githubActionCore.error(
-      `Failed to create PR for next release version bump. Error:\n${error.message}\n`,
+      `Failed to create PR for next release version bump. Error:\n${error.message}\n`
     );
     process.exit(1);
   }
